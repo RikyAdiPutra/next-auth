0fd938debd523c4aff30ffae2962bf7962c887f1
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-08-01 13:04:14 +0700

    setup handler next-auth

--- src/app/api/[...nextauth]/route.js
+++ src/app/api/[...nextauth]/route.js
@@ -1,5 +1,18 @@
 import NextAuth from "next-auth/next";
+import GeoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 
-const handler = NextAuth({});
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_CLIENT,
+    }),
+    GeoogleProvider({
+      clientId: process.env.GEOOGLE_CLIENT_ID,
+      clientSecret: process.env.GEOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
 
 export { handler as GET, handler as POST };
