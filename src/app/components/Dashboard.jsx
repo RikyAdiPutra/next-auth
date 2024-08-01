ed092cae7a9cb81518f3162d1eb4154b0ad6a5b4
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-08-01 17:12:33 +0700

    display property from session in pages

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -3,14 +3,15 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 const Dashboard = () => {
   const { data } = useSession();
-  console.log({ data });
   return (
     <div>
       {data ? (
         <div>
-          <h1>Welcome back</h1>
+          <h1>Welcome back {data.user?.name}</h1>
+          <p>{data.user?.email}</p>
           <button
             onClick={() => signOut({ callbackUrl: "http://localhost:3000" })}
+            className="bg-red-400 text-white p-2 rounded-lg mt-4"
           >
             Kembali
           </button>
@@ -18,7 +19,18 @@ const Dashboard = () => {
       ) : (
         <div>
           <h1>Kamu belum login</h1>
-          <button onClick={() => signIn("github")}>Sign with githb</button>
+          <button
+            onClick={() => signIn("google")}
+            className="bg-red-400 text-white p-2 rounded-lg"
+          >
+            Sign with geogle
+          </button>
+          <button
+            onClick={() => signIn("github")}
+            className="bg-gray-400 text-white p-2 rounded-lg"
+          >
+            Sign with github
+          </button>
         </div>
       )}
     </div>
